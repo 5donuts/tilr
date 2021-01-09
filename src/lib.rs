db4d739880e35f5ab82b2fbc0dee15481a4db58a
Charles German <5donuts@protonmail.com>
2021-01-08 22:48:47 -0500

    Initial fn sigs for the lib

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,17 @@
 
 mod tileset;
 
+use image::RgbImage;
+use std::path::PathBuf;
+
 pub use tileset::TileSet;
+
+pub fn load_tiles(tile_dir: &PathBuf) -> TileSet<'_> {
+    todo!("load all of the images in the dir");
+    todo!("scale them to squares with the smallest side length of any image");
+    todo!("build a TileSet from those images")
+}
+
+pub fn stitch_mosaic(img: &RgbImage, tiles: &TileSet<'_>) -> RgbImage {
+    todo!()
+}
