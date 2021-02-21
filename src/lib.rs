09421138a04ced75895e93f896de0044f64b1373
Charles German <5donuts@protonmail.com>
2021-02-21 15:44:17 -0500

    Wrap mosaic building with a struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,109 @@
 #![warn(
-    // missing_docs,
+    missing_docs,
     rust_2018_idioms,
     missing_debug_implementations,
     broken_intra_doc_links
 )]
 
-mod tileset;
+use image::{DynamicImage, GenericImage, GenericImageView, Pixel, RgbImage};
+use tiles::{Tile, TileSet};
 
-pub use tileset::TileSet;
+mod tiles;
 
-use image::{DynamicImage, GenericImage, Pixel, RgbImage};
-use tileset::Tile;
-
-/// Build a mosaic of an image using the given tile set to map
-/// between pixels in the original image and a tile in the set.
-/// NB: this may take a hot second to run.
-// TODO: implement original image scaling/resulting mosaic scaling
-// to reduce resulting image sizes
-pub fn make_mosaic(img: &RgbImage, tiles: &TileSet) -> RgbImage {
-    // get a mapping of image px to tiles
-    let map = tiles.map_to(&img);
-
-    // compute the size of the resulting mosaic
-    let tile_sz = tiles.tile_side_len();
-    let (img_x, img_y) = img.dimensions();
-    let (mos_x, mos_y) = (img_x * tile_sz, img_y * tile_sz);
-    eprintln!(
-        "Building mosaic ({}, {}) with tiles of size ({}, {}).",
-        mos_x, mos_y, tile_sz, tile_sz
-    );
-
-    // build the mosaic
-    let mut mosaic = DynamicImage::new_rgb8(mos_x, mos_y);
-    let mut mos_x = 0;
-    for x in 0..img_x {
-        let mut mos_y = 0;
-        for y in 0..img_y {
-            // add the tile to the mosaic
-            eprintln!(
-                "Adding tile for img px ({}, {}) to mosaic at ({}, {})...",
-                x, y, mos_x, mos_y
-            );
-            let tile_for_px = map.get(img.get_pixel(x, y)).expect("No tile for px");
-            add_tile_to_mosaic(&mut mosaic, tile_for_px, (mos_x, mos_y));
-            mos_y += tile_sz;
-        }
-        mos_x += tile_sz;
-    }
-
-    // scale the image (TODO: remove, this is for testing)
-    let mosaic = mosaic.resize_exact(1024, 1024, image::imageops::FilterType::Nearest);
-
-    // convert to RGB image and return
-    mosaic.into_rgb8()
+pub struct Mosaic {
+    img: RgbImage,
+    tiles: TileSet,
+    inner: Inner,
 }
 
-fn add_tile_to_mosaic(mosaic: &mut DynamicImage, tile: &Tile, start_coords: (u32, u32)) {
-    let s = tile.side_len();
-    let (start_x, start_y) = start_coords;
-    let mut tile_px = tile.img().pixels();
-    for x in start_x..(start_x + s) {
-        for y in start_y..(start_y + s) {
-            let px = tile_px
-                .next()
-                .expect("Unable to get next tile px")
-                .to_rgba();
-            mosaic.put_pixel(x, y, px);
+impl Mosaic {
+    pub fn new(
+        img: DynamicImage,
+        tiles: &Vec<DynamicImage>,
+        img_scaling: f32,
+        tile_size: u8,
+    ) -> Self {
+        // Scale the source image, if specified
+        let img = if img_scaling != 1.0 {
+            let (x, y) = img.dimensions();
+            let x = (x as f32 * img_scaling) as u32;
+            let y = (y as f32 * img_scaling) as u32;
+            img.resize_exact(x, y, image::imageops::FilterType::Triangle)
+        } else {
+            img
+        }
+        .to_rgb8();
+
+        // Build the tileset
+        let mut tiles = TileSet::from(tiles);
+
+        // Scale the tiles if they're not already appropriately
+        // sized.
+        // TODO: just build them the correct size to start with.
+        let tile_size = tile_size as u32;
+        if tiles.tile_side_len() != tile_size {
+            tiles.scale_tiles(tile_size);
+        }
+
+        // Initialize the inner image (the output mosaic image)
+        let (img_x, img_y) = img.dimensions();
+        let (mos_x, mos_y) = (img_x * tile_size, img_y * tile_size);
+        let inner = Inner(DynamicImage::new_rgb8(mos_x, mos_y));
+        eprintln!(
+            "Building mosaic with size ({}px, {}px) from tiles of side length {}px.",
+            mos_x, mos_y, tile_size
+        );
+
+        Self { img, tiles, inner }
+    }
+
+    pub fn generate(self) -> RgbImage {
+        let map = self.tiles.map_to(&self.img);
+        let (img_x, img_y) = self.img.dimensions();
+        let tile_size = self.tiles.tile_side_len();
+        let mut mosaic = self.inner;
+
+        // Build the mosaic
+        let mut mos_x = 0;
+        for x in 0..img_x {
+            let mut mos_y = 0;
+            for y in 0..img_y {
+                eprintln!(
+                    "Adding tile for img px ({}, {}) to mosaic at ({}, {})...",
+                    x, y, mos_x, mos_y
+                );
+
+                // Add the tile to the mosaic
+                let tile_for_px = map.get(&self.img.get_pixel(x, y)).expect("No tile for px");
+                mosaic.add_tile(tile_for_px, (mos_x, mos_y));
+
+                // Move to the next pixel in the mosaic
+                mos_y += tile_size;
+            }
+
+            // Move to the next row in the mosaic
+            mos_x += tile_size;
+        }
+
+        mosaic.0.into_rgb8()
+    }
+}
+
+struct Inner(DynamicImage);
+
+impl Inner {
+    pub fn add_tile(&mut self, tile: &Tile, start_coords: (u32, u32)) {
+        let s = tile.side_len();
+        let (start_x, start_y) = start_coords;
+        let mut tile_px = tile.img().pixels();
+        for x in start_x..(start_x + s) {
+            for y in start_y..(start_y + s) {
+                let px = tile_px
+                    .next()
+                    .expect("Unable to get next tile px")
+                    .to_rgba();
+                self.0.put_pixel(x, y, px);
+            }
         }
     }
 }
