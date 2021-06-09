71c178d123bd2bf9365308ff1989e6a1e7e0ed99
Charles German <5donuts@protonmail.com>
2021-06-09 17:17:39 -0400

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+//! Build a mosaic of a particular image out of a set of smaller images.
+
 #![warn(
     missing_docs,
     rust_2018_idioms,
@@ -10,24 +12,70 @@ use tiles::{Tile, TileSet};
 
 mod tiles;
 
+/// Generates an image 'mosaic' using a set of image Tiles.
+///
+/// An image 'mosaic' is an image made up of a number of smaller
+/// images in place of pixels. Using the average color of an image
+/// Tile, a suitable large image mosaic viewed from far enough
+/// away can appear to be a normal image.
+#[allow(missing_debug_implementations)]
 pub struct Mosaic {
+    /// The original image used to create the mosaic.
     img: RgbImage,
+    /// The set of [`Tile`]s to use to build the mosaic.
+    ///
+    /// Pixels in the original image are mapped to these tiles based
+    /// on the Euclidean distance between the RGB pixel values and the
+    /// average RGB values in the [`Tile`].
     tiles: TileSet,
+    /// An inner member used to build the resulting image mosaic.
     inner: Inner,
 }
 
 impl Mosaic {
+    /// Initialize a new image mosaic.
+    ///
+    /// # Arguments
+    /// * `img` - The original image used to create the mosaic.
+    /// * `tiles` - The set of Tiles to use to build the mosaic.
+    /// * `img_scaling` - The scaling factor to apply to the original
+    ///                   image for the mosaic. A scaling factor of `1`
+    ///                   means no scaling. The scaling performed does
+    ///                   _not_ preserve aspect ratio.
+    /// * `tile_size` - The desired side length for the Tiles to use
+    ///                 to generate this mosaic. If the Tiles are not
+    ///                 already squares with this side length, they will
+    ///                 be resized (without preserving aspect ratio) to
+    ///                 be squares with the given side length.
+    ///
+    /// # Returns
+    /// An empty mosaic. To build the mosaic, call [`to_image`](Mosaic::to_image).
+    /// Note that generating the resulting mosaic is an expensive operation and
+    /// could take many seconds (or minutes for especially large mosaics).
+    ///
+    /// # Panics
+    /// This function panics if `img_scaling` is less than `0.1`.
+    /// Additionally, it will panic if the chosen scaling factor would result
+    /// in an image that has zero pixels in any dimension.
     pub fn new(
         img: DynamicImage,
         tiles: &Vec<DynamicImage>,
         img_scaling: f32,
         tile_size: u8,
     ) -> Self {
+        if img_scaling < 0.1 {
+            panic!("Scaling factor must be at least 0.1.");
+        }
         // Scale the source image, if specified
         let img = if img_scaling != 1.0 {
             let (x, y) = img.dimensions();
             let x = (x as f32 * img_scaling) as u32;
             let y = (y as f32 * img_scaling) as u32;
+            if x == 0 || y == 0 {
+                panic!(
+                    "Scaling factor results in an image with at least one dimension with zero px"
+                );
+            }
             img.resize_exact(x, y, image::imageops::FilterType::Triangle)
         } else {
             img
@@ -57,7 +105,11 @@ impl Mosaic {
         Self { img, tiles, inner }
     }
 
-    pub fn generate(self) -> RgbImage {
+    /// Generate the image mosaic and convert it to an [`RgbImage`].
+    ///
+    /// Depending on the size of the mosaic to build, this function may
+    /// take some time to run.
+    pub fn to_image(self) -> RgbImage {
         let map = self.tiles.map_to(&self.img);
         let (img_x, img_y) = self.img.dimensions();
         let tile_size = self.tiles.tile_side_len();
@@ -89,9 +141,16 @@ impl Mosaic {
     }
 }
 
+/// A wrapper around a [`DynamicImage`] used to build the resulting
+/// image mosaic.
 struct Inner(DynamicImage);
 
 impl Inner {
+    /// Add a [`Tile`] to the image mosaic.
+    ///
+    /// More specifically, insert the pixels of a given [`Tile`] into
+    /// this image at an offset based on where that [`Tile`] belongs
+    /// in the [`Mosaic`].
     pub fn add_tile(&mut self, tile: &Tile, start_coords: (u32, u32)) {
         let s = tile.side_len();
         let (start_x, start_y) = start_coords;
